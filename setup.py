008fc4bdd99552df59c2541e70b4b3fad2a4c8b0
Drew Bonasera <Drewsif@users.noreply.github.com>
2015-05-03 03:15:06 -0400

    Is this how I setup.py?

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='PySecretSOCKS',
+    version='0.8',
+    packages=['secretsocks'],
+    url='https://github.com/Drewsif/PySecretSOCKS',
+    license='MIT',
+    author='Drew Bonasera',
+    author_email='',
+    description='A python SOCKS server for tunneling connections over another channel. Making implementing covert channels a breeze!',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Security'
+    ]
+)
